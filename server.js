b1756543b385f5357008f275b60aaa874b504a92
Brandao-lang <elijahbrandao@gmail.com>
2021-06-15 14:02:54 -0400

    fixed endpoint

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const Rollbar = require('rollbar')
 const app = express()
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served succesfully')
+})
+
 
 
 
